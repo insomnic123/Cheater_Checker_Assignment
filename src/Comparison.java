723ebf44a32589f079a0b8d108a6f4fc2227beb7
insomnic123 <qaziayn@gmail.com>
2024-11-13 21:48:07 -0500

    added a bunch rahhh

--- src/Comparison.java
+++ src/Comparison.java
@@ -13,17 +13,40 @@ public class Comparison {
     private int curlyBracketCount = 0;
     private int regularBracketCount = 0;
     private int lineCount = 0;
+    private int numRegions = 0; // https://www.youtube.com/watch?v=edjOiohPPw8&ab_channel=TahiaTabassum
+    private int elseIfCount = 0;
+    private int whileCount = 0;
+    private int doWhileCount = 0;
+    private int caseCount = 0;
+    private int forCount = 0;
 
-    private int ifCountDiv = 0;
-    private int curlyBracketCountDiv = 0;
-    private int regularBracketCountDiv = 0;
-    private int lineCountDiv = 0;
+    private double ifCountDiv = 0;
+    private double curlyBracketCountDiv = 0;
+    private double regularBracketCountDiv = 0;
+    private double lineCountDiv = 0;
+    private double numRegionsDiv = 0;
+    private double elseIfCountDiv = 0;
+    private double whileCountDiv = 0;
+    private double doWhileCountDiv = 0;
+    private double caseCountDiv = 0;
+    private double forCountDiv = 0;
 
-    public Comparison(int lineCount, int ifCount, int curlyBracketCount, int regularBracketCount) {
+    public Comparison(int lineCount, int ifCount, int curlyBracketCount, int regularBracketCount, int forCount, int
+                      caseCount, int whileCount, int doWhileCount, int elseIfCount, int numRegions) {
         this.lineCount = lineCount;
         this.ifCount = ifCount;
-        this.curlyBracketCount = curlyBracketCount;
-        this.regularBracketCount = regularBracketCount;
+        this.curlyBracketCount = curlyBracketCount*2;
+        this.regularBracketCount = regularBracketCount*2;
+        this.forCount = forCount;
+        this.caseCount = caseCount;
+        this.whileCount = whileCount;
+        this.doWhileCount = doWhileCount;
+        this.elseIfCount = elseIfCount;
+        this.numRegions = numRegions;
+    }
+
+    public double getPercentDiff(int valueA, int valueB) {
+        return (Math.abs(valueA - valueB) / (double) valueA)*100;
     }
 
     public int getIfCount() {
@@ -58,7 +81,7 @@ public class Comparison {
         this.lineCount = lineCount;
     }
 
-    public int getIfCountDiv() {
+    public double getIfCountDiv() {
         return ifCountDiv;
     }
 
@@ -66,7 +89,7 @@ public class Comparison {
         this.ifCountDiv = ifCountDiv;
     }
 
-    public int getCurlyBracketCountDiv() {
+    public double getCurlyBracketCountDiv() {
         return curlyBracketCountDiv;
     }
 
@@ -74,7 +97,7 @@ public class Comparison {
         this.curlyBracketCountDiv = curlyBracketCountDiv;
     }
 
-    public int getLineCountDiv() {
+    public double getLineCountDiv() {
         return lineCountDiv;
     }
 
@@ -82,11 +105,134 @@ public class Comparison {
         this.lineCountDiv = lineCountDiv;
     }
 
-    public int getRegularBracketCountDiv() {
+    public double getRegularBracketCountDiv() {
         return regularBracketCountDiv;
     }
 
     public void setRegularBracketCountDiv(int regularBracketCountDiv) {
         this.regularBracketCountDiv = regularBracketCountDiv;
+    }
+
+    public String toString() {
+        return ("Num Lines: " + lineCount + " | if Count: " + ifCount + " | curlyBracketCount: " + curlyBracketCount
+        + " | regBracketCount: " + regularBracketCount);
+    }
+
+    public void deviation(Comparison x) {
+        lineCountDiv = getPercentDiff(this.lineCount, x.getLineCount());
+        System.out.printf("The line count differs by %.2f%%%n", lineCountDiv);
+
+        ifCountDiv = getPercentDiff(this.ifCount, x.getIfCount());
+        System.out.printf("The if count differs by %.2f%%$n", ifCountDiv);
+
+        curlyBracketCountDiv = getPercentDiff(this.curlyBracketCount, x.getCurlyBracketCount());
+        System.out.printf("The number of curly brackets differ by %.2f%%%n", curlyBracketCountDiv);
+
+        regularBracketCountDiv = getPercentDiff(this.regularBracketCount, x.getRegularBracketCount());
+        System.out.printf("The number of regular brackets differ by %.2f%%%n", regularBracketCountDiv);
+
+        numRegionsDiv = getPercentDiff(this.numRegions + 1, x.getNumRegions() + 1);
+        System.out.printf("The Cyclomatic Complexity differs by %.2f%%%n", numRegionsDiv);
+
+        double average = (lineCountDiv + ifCountDiv + curlyBracketCountDiv + regularBracketCountDiv) / 4;
+
+        System.out.printf("The average deviation is: %.2f%%%n", average);
+
+    }
+
+    public double getNumRegionsDiv() {
+        return numRegionsDiv;
+    }
+
+    public void setNumRegionsDiv(double numRegionsDiv) {
+        this.numRegionsDiv = numRegionsDiv;
+    }
+
+    public double getElseIfCountDiv() {
+        return elseIfCountDiv;
+    }
+
+    public void setElseIfCountDiv(double elseIfCountDiv) {
+        this.elseIfCountDiv = elseIfCountDiv;
+    }
+
+    public double getWhileCountDiv() {
+        return whileCountDiv;
+    }
+
+    public void setWhileCountDiv(double whileCountDiv) {
+        this.whileCountDiv = whileCountDiv;
+    }
+
+    public double getDoWhileCountDiv() {
+        return doWhileCountDiv;
+    }
+
+    public void setDoWhileCountDiv(double doWhileCountDiv) {
+        this.doWhileCountDiv = doWhileCountDiv;
+    }
+
+    public double getCaseCountDiv() {
+        return caseCountDiv;
+    }
+
+    public void setCaseCountDiv(double caseCountDiv) {
+        this.caseCountDiv = caseCountDiv;
+    }
+
+    public double getForCountDiv() {
+        return forCountDiv;
+    }
+
+    public void setForCountDiv(double forCountDiv) {
+        this.forCountDiv = forCountDiv;
+    }
+
+    public int getForCount() {
+        return forCount;
+    }
+
+    public void setForCount(int forCount) {
+        this.forCount = forCount;
+    }
+
+    public int getCaseCount() {
+        return caseCount;
+    }
+
+    public void setCaseCount(int caseCount) {
+        this.caseCount = caseCount;
+    }
+
+    public int getWhileCount() {
+        return whileCount;
+    }
+
+    public void setWhileCount(int whileCount) {
+        this.whileCount = whileCount;
+    }
+
+    public int getNumRegions() {
+        return numRegions;
+    }
+
+    public void setNumRegions(int numRegions) {
+        this.numRegions = numRegions;
+    }
+
+    public int getElseIfCount() {
+        return elseIfCount;
+    }
+
+    public void setElseIfCount(int elseIfCount) {
+        this.elseIfCount = elseIfCount;
+    }
+
+    public int getDoWhileCount() {
+        return doWhileCount;
+    }
+
+    public void setDoWhileCount(int doWhileCount) {
+        this.doWhileCount = doWhileCount;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,12 @@ public class Main {
     public static int curlyBracketCount = 0;
     public static int regularBracketCount = 0;
     public static int lineCount = 0;
+    public static int numRegions = 0; // https://www.youtube.com/watch?v=edjOiohPPw8&ab_channel=TahiaTabassum
+    public static int elseIfCount = 0;
+    public static int whileCount = 0;
+    public static int doWhileCount = 0;
+    public static int caseCount = 0;
+    public static int forCount = 0;
 
     // Colours from https://www.mymiller.name/wordpress/java/ansi-colors/
     public static final String RESET = "\u001B[0m";
@@ -47,6 +53,32 @@ public class Main {
 
                 if (line.contains("if")) {
                     ifCount++;
+                    numRegions++;
+                }
+
+                if (line.contains("else if")) {
+                    elseIfCount++;
+                    numRegions++;
+                }
+
+                if (line.contains("while")) {
+                    whileCount++;
+                    numRegions++;
+                }
+
+                if (line.contains("do")) {
+                    doWhileCount++;
+                    numRegions++;
+                }
+
+                if (line.contains("for")) {
+                    forCount++;
+                    numRegions++;
+                }
+
+                if (line.contains("case")) {
+                    caseCount++;
+                    numRegions++;
                 }
 
                 if (line.contains("{")) {
@@ -66,6 +98,19 @@ public class Main {
         }
     }
 
+    public static void valueReset() {
+        lineCount = 0;
+        ifCount = 0;
+        curlyBracketCount = 0;
+        regularBracketCount = 0;
+        forCount = 0;
+        caseCount = 0;
+        whileCount = 0;
+        doWhileCount = 0;
+        elseIfCount = 0;
+        numRegions = 0;
+    }
+
     // Print statement because no one likes Java
     public static void print(String msg) {
         System.out.println(msg);
@@ -73,7 +118,24 @@ public class Main {
 
     public static void main(String[] args) {
         processData("C:\\Users\\qazia\\Desktop\\CS12\\Cheater Checker Assignmnt\\src\\SumOfMultiples1.java");
-        Comparison fileA = new Comparison(lineCount, ifCount, curlyBracketCount, regularBracketCount);
+        Comparison fileA = new Comparison(lineCount, ifCount, curlyBracketCount, regularBracketCount, forCount, caseCount,
+                whileCount, doWhileCount, elseIfCount, numRegions);
+        valueReset();
+        print(String.valueOf(fileA));
+        print("-----------------------------------------------");
+        processData("C:\\Users\\qazia\\Desktop\\CS12\\Cheater Checker Assignmnt\\src\\SumOfMultiples2.java");
+        Comparison fileB = new Comparison(lineCount, ifCount, curlyBracketCount, regularBracketCount, forCount, caseCount,
+                whileCount, doWhileCount, elseIfCount, numRegions);
+        valueReset();
+        print(String.valueOf(fileB));
+        print("-----------------------------------------------");
+        processData("C:\\Users\\qazia\\Desktop\\CS12\\Cheater Checker Assignmnt\\src\\SumOfMultiples3.java");
+        Comparison fileC = new Comparison(lineCount, ifCount, curlyBracketCount, regularBracketCount, forCount, caseCount,
+                whileCount, doWhileCount, elseIfCount, numRegions);
+        valueReset();
+        print(String.valueOf(fileC));
+
+        fileA.deviation(fileC);
 
     }
 }
